35c65001e2947864fba91bb06d0f1f18f9bea54e
seeflood <349895584@qq.com>
2021-12-31 23:12:11 +0800

    two modes

--- add_crontab.sh
+++ add_crontab.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-crontab -l > cron_bkp
+# save your existing crontab tasks
+# crontab -l > cron_bkp
+
 # open /Applications/DingTalk.app
-echo "0 */1 * * * open /Applications/DingTalk.app -j" >> cron_bkp
+if [ $1 == "b" ]
+then
+  echo "Add a crontab job: open dingtalk in background"
+   echo "0 */1 * * * open /Applications/DingTalk.app -j" > cron_bkp
+else
+  echo "Add a crontab job: open dingtalk in foreground"
+   echo "0 */1 * * * open /Applications/DingTalk.app" > cron_bkp
+fi
+
 # killall DingTalk
 echo "5 */1 * * * killall DingTalk" >> cron_bkp
 crontab cron_bkp
 rm cron_bkp
+echo "killall DingTalk ..."
+killall DingTalk
+echo "Done!"
+echo "You can use 'crontab -l' to check your crontab jobs"
